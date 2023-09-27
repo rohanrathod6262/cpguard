bbe4690e7f95cb65bc756a206f76c52c5851afdd
pravinpimpale <pravinpimpale45@gmail.com>
2023-09-27 16:16:35 +0530

    initial commit

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyA1w6oV9cEok-GljuQZf3ATCH7x4dP87Po",
+  authDomain: "chatapp-a09bc.firebaseapp.com",
+  projectId: "chatapp-a09bc",
+  storageBucket: "chatapp-a09bc.appspot.com",
+  messagingSenderId: "13954429",
+  appId: "1:13954429:web:1561bd5d1b926c5470f014",
+  measurementId: "G-4Z9CNQSX2E"
+  //   @deprecated is deprecated Constants.manifest
+};
+// initialize firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
