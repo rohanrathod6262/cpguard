02d45781c6221170343fcaaca225604d854a9bb9
rohanrathod6262 <rohanrathod6262@gmail.com>
2023-09-28 15:44:46 +0530

    home page updated

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA1w6oV9cEok-GljuQZf3ATCH7x4dP87Po",
-  authDomain: "chatapp-a09bc.firebaseapp.com",
-  projectId: "chatapp-a09bc",
-  storageBucket: "chatapp-a09bc.appspot.com",
-  messagingSenderId: "13954429",
-  appId: "1:13954429:web:1561bd5d1b926c5470f014",
-  measurementId: "G-4Z9CNQSX2E"
+  apiKey: "AIzaSyAW0AkjQ-VOrOuJqSIMjxTSCfTjas6kIpA",
+  authDomain: "cpguard-ced66.firebaseapp.com",
+  projectId: "cpguard-ced66",
+  storageBucket: "cpguard-ced66.appspot.com",
+  messagingSenderId: "977050612005",
+  appId: "1:977050612005:web:820d0cc8631215a3deaacc",
+  measurementId: "G-48525FVLXT"
   //   @deprecated is deprecated Constants.manifest
 };
 // initialize firebase
