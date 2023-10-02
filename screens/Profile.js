0f0e0d0ecab24841f7650b899d0757131bf81318
rohanrathod6262 <rohanrathod6262@gmail.com>
2023-10-02 16:56:34 +0530

    updated

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,76 +1,86 @@
 import React, { useEffect } from "react";
-import { View, TouchableOpacity, Button, Text, Image, StyleSheet, backgroundColor, ImageBackground} from "react-native";
+import { View, TouchableOpacity, Button, Text, Image, StyleSheet, backgroundColor, ImageBackground } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Footer from "./component/Footer";
 import EditProfile from "./EditProfile";
+import { Switch } from '@rneui/themed';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
     const navigation = useNavigation();
 
-    const handleYesPress = () => {
-        console.log('Yes button pressed!');
-        // Handle your logic for "Yes" here
-    };
+    //  const [checked, setChecked] = useState(false);
 
-    const handleNoPress = () => {
-        console.log('No button pressed!');
-        // Handle your logic for "No" here
-    };
+    //  const toggleSwitch = () => {
+    //    setChecked(!checked);
+    //  };
+
+
+    // const handleNoPress = () => {
+    //     console.log('No button pressed!');
+    //     // Handle your logic for "No" here
+    // };
 
     return (
         <>
-           <View>
-              
-                <ImageBackground source={require('../assets/backImage.jpeg')} resizeMode="cover" style={styles.image1}>
-                
-            <TouchableOpacity style={styles.buttonContainer}>
-                        <Button  onPress={() => navigation.navigate("EditProfile")} style={{
-                        paddingVertical: 10,
-                        paddingHorizontal: 20,
-                        borderRadius: 20
-                    }}
-                        title="Edit Profile" />
-                    </TouchableOpacity> 
-                
-                
-                <View style={styles.container}>
-                    <Image source={require('../assets/profile.webp')} style={styles.image} />
-                </View>
+        <View style={{ flex: 1,backgroundColor: '#FFE8F3' }}>
+
+           
+
+                <ImageBackground source={require('../assets/backImage.jpeg')} resizeMode="cover" style={styles.image}>
+
+                    <View style={{ flexDirection: 'row', marginTop: 20, marginRight: 10, justifyContent: 'flex-end', }}>
+                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("EditProfile")}>
+                            <Text style={styles.buttonText}>Edit Profile</Text>
+                        </TouchableOpacity>
+                    </View>
+
+
+
+                    <View style={styles.container}>
+                        <Image source={require('../assets/profile.webp')} style={styles.image1} />
+                    </View>
                 </ImageBackground>
 
                 <View style={styles.form}>
-                <Text
-                    style={{
-                        borderColor: "gray",
-                        borderWidth: 0.9,
-                        height: 1,
+                    <Text
+                        style={{
+                            borderColor: "gray",
+                            borderWidth: 0.9,
+                            height: 1,
 
-                    }} />
-                <View style={styles.RectangleShapeView}>
-                <Text style={styles.name}>Name: Rohan </Text>
-               
-                <Text style={styles.number}>Number: 7887308177</Text>
-                <Text style={styles.email}>Email: rohan6262@gmail.com</Text>
-                <Text style={styles.number}>password:  ****</Text>
-               
-                {/* <Text style={styles.bio}>{user.bio}</Text>
-            Add more user details if needed */}
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={styles.privacyy}>Privacy:</Text>
-                    <View style={{ marginLeft: 10, flexDirection: 'row' }}>
-                        <Button title="On" onPress={handleYesPress} />
-                        <Button title="Off" onPress={handleNoPress} />
+                        }} />
+
+                    <View style={styles.RectangleShapeView}>
+                        <Text style={styles.name}>Name: Rohan </Text>
+
+                        <Text style={styles.number}>Number: 7887308177</Text>
+                        <Text style={styles.email}>Email: rohan6262@gmail.com</Text>
+                        <Text style={styles.number}>password:  ****</Text>
+
+
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={styles.privacyy}>Privacy:</Text>
+                            <Switch style={{marginLeft: 10, marginTop:7}}
+                                // value={checked}
+                                // onValueChange={(value) => setChecked(value)}
+                            />
+
+
+                           
+                        </View>
+
                     </View>
-                </View>
-                </View>
-                </View>
-                </View>
-               
 
-                
+
+                </View> 
+            </View>s
+
+
 
 
             <Footer />
+        
         </>
     )
 }
@@ -84,7 +94,7 @@ const styles = StyleSheet.create({
         flex: 1,
 
         alignItems: 'center',
-        
+
         padding: 20
     },
 
@@ -94,19 +104,39 @@ const styles = StyleSheet.create({
         padding: 16,//
         borderRadius: 20,
     },
-    form: {
-        width: '80%',
-        marginTop:80,
-        marginLeft:40,
-        justifyContent: 'flex-start',
-      },
 
-      image1: {
-        width: 400,
-        height: 168,
-        
+    button: {
+
+        flexDirection: 'row',     // sets the primary axis to horizontal
+        justifyContent: 'flex-end',
+        backgroundColor: '#007FFF',
+        borderRadius: 10,
+
+        justifyContent: 'center',
+        width: 120,
+        height: 40,
+
 
     },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        padding: 10
+
+    },
+    form: {
+        width: '80%',
+        marginTop: 80,
+        marginLeft: 40,
+       // justifyContent: 'flex-start',
+    },
+
+    image: {
+        width: 400,
+        height: 168,
+    },
+
     RectangleShapeView: {
         marginTop: 10,
         width: "100%",
@@ -117,35 +147,36 @@ const styles = StyleSheet.create({
         borderColor: "black",
         borderWidth: 1,
         elevation: 3
-      },
-      name: {
-        paddingLeft:10,
+    },
+    name: {
+        paddingLeft: 10,
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 20,
         marginTop: 2,
 
     },
-    image: {
+    image1: {
         width: 100,
         height: 100,
-        
+
         borderRadius: 40,
         marginBottom: 10,
         marginTop: 30,
 
     },
-    
+
     number: {
-        paddingLeft:10,
+        paddingLeft: 10,
         fontSize: 20,
         marginBottom: 20,
         fontWeight: 'bold',
 
     },
 
+
     email: {
-        paddingLeft:10,
+        paddingLeft: 10,
         fontSize: 20,
         marginBottom: 20,
         fontWeight: 'bold',
@@ -154,7 +185,7 @@ const styles = StyleSheet.create({
     },
 
     privacyy: {
-        paddingLeft:10,
+        paddingLeft: 10,
         fontSize: 20,
         fontWeight: 'bold',
 
@@ -163,3 +194,8 @@ const styles = StyleSheet.create({
     },
 
 });
+
+ {/* <View style={{ marginLeft: 10, flexDirection: 'row' }}>
+                                <Button title="On" onPress={handleYesPress} />
+                                <Button title="Off" onPress={handleNoPress} />
+                            </View> */}
